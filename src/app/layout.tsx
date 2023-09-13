35959f3599b45eda6a997b58d9cc4e2b1df9fe9b
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-09-13 11:29:35 -0300

    fix import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "../styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Navbar } from "@/components/header";
+import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
