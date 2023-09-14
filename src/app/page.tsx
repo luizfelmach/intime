f81155d0f43499cec80a2f4b28047f971c241242
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-09-14 18:43:52 -0300

    add copy about in time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -137,94 +137,109 @@ export default function Home() {
                   Sobre a In Time Relógios.
                 </h1>
 
-                <div className="space-y-16">
-                  <div className="flex justify-start gap-4">
-                    <Image
-                      className="verysmall:hidden"
-                      src={"/about/quality.svg"}
-                      alt="Qualidade da In Time Relógios"
-                      height={110}
-                      width={110}
-                    />
-                    <div>
-                      <h1 className="scroll-m-20 text-background/50  pb-2 text-2xl font-black tracking-tight transition-colors mb-8 max-w-[400px] whitespace-normal">
-                        Qualidade.
-                      </h1>
-                      <p className="text-right max-w-[500px] leading-7 [&:not(:first-child)]:mt-6 whitespace-normal font-semibold text-background/50">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Hic eius labore adipisci ea sequi ullam maxime
-                        libero illum consequatur consectetur dignissimos, porro,
-                        repudiandae doloribus quibusdam voluptate sint corporis
-                        nemo in?
-                      </p>
+                <div className="space-y-16 overflow-hidden">
+                  <Slide triggerOnce direction="left">
+                    <div className="flex justify-start gap-4">
+                      <Image
+                        className="verysmall:hidden"
+                        src={"/about/quality.svg"}
+                        alt="Qualidade da In Time Relógios"
+                        height={110}
+                        width={110}
+                      />
+                      <div>
+                        <h1 className="scroll-m-20 text-background/50  pb-2 text-2xl font-black tracking-tight transition-colors mb-8 max-w-[400px] whitespace-normal">
+                          Qualidade.
+                        </h1>
+                        <p className="text-right max-w-[500px] leading-7 [&:not(:first-child)]:mt-6 whitespace-normal font-semibold text-background/50">
+                          Nós acreditamos que um relógio não é apenas um
+                          acessório, mas uma obra de arte que deve durar
+                          gerações. É por isso que escolhemos trabalhar com
+                          marcas renomadas conhecidas por seu artesanato e
+                          qualidade.
+                        </p>
+                      </div>
                     </div>
-                  </div>
+                  </Slide>
 
-                  <div className="flex justify-end gap-4">
-                    <div>
-                      <h1 className="scroll-m-20 text-background/50  pb-2 text-2xl font-black tracking-tight transition-colors mb-8 max-w-[400px] whitespace-normal">
-                        Atendimento.
-                      </h1>
-                      <p className="max-w-[500px] leading-7 [&:not(:first-child)]:mt-6 whitespace-normal font-semibold text-background/50">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Hic eius labore adipisci ea sequi ullam maxime
-                        libero illum consequatur consectetur dignissimos, porro,
-                        repudiandae doloribus quibusdam voluptate sint corporis
-                        nemo in?
-                      </p>
+                  <Slide triggerOnce direction="right">
+                    <div className="flex justify-end gap-4">
+                      <div>
+                        <h1 className="scroll-m-20 text-background/50  pb-2 text-2xl font-black tracking-tight transition-colors mb-8 max-w-[400px] whitespace-normal">
+                          Atendimento.
+                        </h1>
+                        <p className="max-w-[500px] leading-7 [&:not(:first-child)]:mt-6 whitespace-normal font-semibold text-background/50">
+                          Nossa equipe dedicada está aqui para ajudar você a
+                          encontrar o relógio perfeito. Estamos prontos para
+                          responder a todas as suas perguntas e oferecer
+                          orientação personalizada para sua escolha. Experimente
+                          nos chamar no WhatsApp.
+                        </p>
+                      </div>
+                      <Image
+                        className="verysmall:hidden"
+                        src={"/about/service.svg"}
+                        alt="Qualidade da In Time Relógios"
+                        height={120}
+                        width={120}
+                      />
                     </div>
-                    <Image
-                      className="verysmall:hidden"
-                      src={"/about/service.svg"}
-                      alt="Qualidade da In Time Relógios"
-                      height={120}
-                      width={120}
-                    />
-                  </div>
+                  </Slide>
 
-                  <div className="flex justify-start gap-4">
-                    <Image
-                      className="verysmall:hidden"
-                      src={"/about/shipping.svg"}
-                      alt="Qualidade da In Time Relógios"
-                      height={120}
-                      width={120}
-                    />
-                    <div>
-                      <h1 className="scroll-m-20 text-background/50  pb-2 text-2xl tracking-tight font-black transition-colors mb-8 max-w-[400px] whitespace-normal">
-                        Entrega.
-                      </h1>
-                      <p className="text-right max-w-[500px] leading-7 [&:not(:first-child)]:mt-6 whitespace-normal text-background/50 font-semibold">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Hic eius labore adipisci ea sequi ullam maxime
-                        libero illum consequatur consectetur dignissimos, porro,
-                        repudiandae doloribus quibusdam voluptate sint corporis
-                        nemo in?
-                      </p>
+                  <Slide triggerOnce direction="left">
+                    <div className="flex justify-start gap-4">
+                      <Image
+                        className="verysmall:hidden"
+                        src={"/about/shipping.svg"}
+                        alt="Qualidade da In Time Relógios"
+                        height={120}
+                        width={120}
+                      />
+                      <div>
+                        <h1 className="scroll-m-20 text-background/50  pb-2 text-2xl tracking-tight font-black transition-colors mb-8 max-w-[400px] whitespace-normal">
+                          Entrega.
+                        </h1>
+                        <p className="text-right max-w-[500px] leading-7 [&:not(:first-child)]:mt-6 whitespace-normal text-background/50 font-semibold">
+                          Após a confirmação do pagamento, enviamos sua
+                          mercadoria aos Correios no próximo dia útil e
+                          fornecemos o código de rastreamento no final do dia. O
+                          prazo de entrega varia de acordo com a modalidade de
+                          frete escolhida pelo cliente (PAC, Sedex ou Sedex 10),
+                          e você pode verificar o valor e o tempo estimado de
+                          entrega usando nosso calculador de frete. Após a
+                          compra, você receberá um link com o código de
+                          rastreamento para acompanhar o envio de sua
+                          mercadoria, desde a postagem até a entrega. Garantimos
+                          uma experiência de compra eficiente e rastreável.
+                        </p>
+                      </div>
                     </div>
-                  </div>
+                  </Slide>
 
-                  <div className="flex justify-end gap-4">
-                    <div>
-                      <h1 className="scroll-m-20 text-background/50 pb-2 text-2xl font-black tracking-tight transition-colors mb-8 max-w-[400px] whitespace-normal">
-                        Garantia.
-                      </h1>
-                      <p className="max-w-[500px] leading-7 [&:not(:first-child)]:mt-6 whitespace-normal text-background/50 font-semibold">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Hic eius labore adipisci ea sequi ullam maxime
-                        libero illum consequatur consectetur dignissimos, porro,
-                        repudiandae doloribus quibusdam voluptate sint corporis
-                        nemo in?
-                      </p>
+                  <Slide triggerOnce direction="right">
+                    <div className="flex justify-end gap-4">
+                      <div>
+                        <h1 className="scroll-m-20 text-background/50 pb-2 text-2xl font-black tracking-tight transition-colors mb-8 max-w-[400px] whitespace-normal">
+                          Garantia.
+                        </h1>
+                        <p className="max-w-[500px] leading-7 [&:not(:first-child)]:mt-6 whitespace-normal text-background/50 font-semibold">
+                          Oferecemos uma garantia de 1 ano para todos os nossos
+                          relógios, cobrindo o maquinário. Após o envio do seu
+                          relógio, você receberá um certificado de garantia
+                          digital, garantindo a qualidade e o funcionamento do
+                          maquinário por 1 ano. Garantimos a tranquilidade do
+                          cliente com nossa política de garantia abrangente.
+                        </p>
+                      </div>
+                      <Image
+                        className="verysmall:hidden"
+                        src={"/about/guarantee.svg"}
+                        alt="Qualidade da In Time Relógios"
+                        height={60}
+                        width={60}
+                      />
                     </div>
-                    <Image
-                      className="verysmall:hidden"
-                      src={"/about/guarantee.svg"}
-                      alt="Qualidade da In Time Relógios"
-                      height={60}
-                      width={60}
-                    />
-                  </div>
+                  </Slide>
                 </div>
               </div>
             </div>
