91a65e95a3c52a0a1f737f711a6835cb7b61b282
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-09-14 18:09:02 -0300

    fix style navbar with padding

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,7 @@ import {
 
 export function Navbar() {
   return (
-    <header className=" bg-background/90 backdrop-blur-3xl fixed top-0 left-0 right-0 z-10">
+    <header className=" bg-background/70 p-3 backdrop-blur-2xl fixed top-0 left-0 right-0 z-10">
       <nav className="max-w-[1300px] w-[100%] relative mx-auto px-4">
         <div className="flex justify-between">
           <div className="self-center">
