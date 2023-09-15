e15dded9edb5c91d157ecaf210518612dfbb8189
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-09-15 12:38:55 -0300

    add logo to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,14 +9,15 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "./ui/navigation-menu";
+import Image from "next/image";
 
 export function Navbar() {
   return (
     <header className=" bg-background/70 p-3 backdrop-blur-2xl fixed top-0 left-0 right-0 z-10">
       <nav className="max-w-[1300px] w-[100%] relative mx-auto px-4">
-        <div className="flex justify-between">
+        <div className="flex justify-between h-10">
           <div className="self-center">
-            <Clock4 />
+            <img src="/logo.svg" alt="In Time Relógios" className="h-20" />
           </div>
           <div>
             <NavigationMenu>
