f90f800f87f5e5ebd2252ab35ec30a87b9bc841d
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-09-15 12:46:51 -0300

    fix alignment

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -16,8 +16,13 @@ export function Navbar() {
     <header className=" bg-background/70 p-3 backdrop-blur-2xl fixed top-0 left-0 right-0 z-10">
       <nav className="max-w-[1300px] w-[100%] relative mx-auto px-4">
         <div className="flex justify-between h-10">
-          <div className="self-center">
-            <img src="/logo.svg" alt="In Time Relógios" className="h-20" />
+          <div className="self-center pt-2">
+            <Image
+              src="/logo.svg"
+              alt="In Time Relógios"
+              width={140}
+              height={140}
+            />
           </div>
           <div>
             <NavigationMenu>
