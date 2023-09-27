26849daf57550fd2f480ed8e22e90de5fe3fbb04
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-09-27 20:22:54 -0300

    update

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,10 +1,7 @@
-import { Clock } from "@/components/clock";
-import { Logo } from "@/components/logo";
-
 export default function Teste() {
   return (
     <div className="h-screen">
-      <h1 className="pt-20">Store</h1>
+      <h1 className="pt-20">Store </h1>
     </div>
   );
 }
