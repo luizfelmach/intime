0165c45611edec5dd3b5b641d7d644e84bd64084
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-09-27 12:32:43 -0300

    add route

--- src/app/api/watch/route.ts
+++ src/app/api/watch/route.ts
@@ -1,10 +1,10 @@
+import { Watch } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
-    return NextResponse.json({
-      name: "In Time Relógios",
-    });
+    const watchs: Watch[] = [];
+    return NextResponse.json(watchs);
   } catch (error: any) {
     return NextResponse.json({
       status: "error",
