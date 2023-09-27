cec9959f00bb36460841268854b724b99726e561
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-09-27 20:42:35 -0300

    first commit of page store

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { Watch } from "@prisma/client";
+import { useEffect, useState } from "react";
+
 export default function Store() {
+  const [watchs, setWatchs] = useState<Watch[]>([]);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch("/api/watch");
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await res.json();
+      setWatchs(data);
+      return data;
+    }
+    getData();
+  }, []);
+
   return (
     <div className="h-screen">
-      <h1 className="pt-20">Store </h1>
+      {watchs.map((watch) => {
+        return <div key={watch.id}>{watch.name}</div>;
+      })}
     </div>
   );
 }
