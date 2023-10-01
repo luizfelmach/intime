f3cd83d5cdaad69bfa294fd4ce4199010269e4a0
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-10-01 19:31:53 -0300

    fix

--- src/adapters/zod/load-watchs-validation.ts
+++ src/adapters/zod/load-watchs-validation.ts
@@ -3,11 +3,11 @@ import { loadWatchsValidation } from "@/helpers/load-watchs-validation";
 import { z } from "zod";
 
 const loadsWatchsSchema = z.object({
-  p: z.number().min(0).optional(),
+  p: z.preprocess(Number, z.number()).optional(),
   brand: z.string().optional(),
   category: z.string().optional(),
-  priceMax: z.number().optional(),
-  priceMin: z.number().optional(),
+  priceMax: z.preprocess(Number, z.number()).optional(),
+  priceMin: z.preprocess(Number, z.number()).optional(),
 });
 
 export class ZodLoadWatchsValidation implements loadWatchsValidation {
